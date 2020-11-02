cf59645cc6893df153c03495e6e706c5cb22a50d
351510443@qq.com <351510443@qq.com>
2020-11-02 23:18:43 +0800

    我的(vip状态),updata

--- src/components/mine/mine-page.vue
+++ src/components/mine/mine-page.vue
@@ -18,6 +18,18 @@
             登录后可以使用更多精彩功能哟
           </span>
         </div>
+      </div>
+      <!--vip狀態-->
+      <div class="status">
+        <div class="status-top">
+          vip状态:未开通
+        </div>
+        <mt-button type="primary" size="small" class="button">
+          签到
+        </mt-button>
+
+        <!--分隔虚线-->
+        <cross-line/>
 
       </div>
 
@@ -27,6 +39,7 @@
 </template>
 
 <script>
+
 export default {
   name: "mine-page",
   methods: {
@@ -39,6 +52,118 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+.mine {
+  width: 100%;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
 
+  .top {
+    flex-direction: column;
+    display: flex;
+
+    .info {
+      background-color: #06c1ae;
+      width: 100%;
+      flex: 1;
+      display: flex;
+      flex-direction: row;
+
+      .avatar-wrapper {
+        margin-top: 20px;
+        margin-bottom: 10px;
+        width: 70px;
+        height: 70px;
+        margin-left: 10px;
+
+        img {
+          width: 60px;
+          height: 60px;
+          border-radius: 50%;
+        }
+      }
+
+      .name-wrapper {
+        display: flex;
+        flex-direction: column;
+        height: 0px;
+        margin-top: 35px;
+
+        .name {
+          font-size: 14px;
+          color: #fff;
+        }
+
+        .desc {
+          font-size: 10px;
+          color: #fcfefe;
+        }
+      }
+    }
+
+    .operation {
+      flex: 1;
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
+
+      i {
+        margin-top: 10px;
+        margin-right: 15px;
+        color: #fff;
+        font-size: 18px;
+        font-weight: 500;
+      }
+    }
+  }
+
+  .important {
+    width: 100%;
+  }
+
+  .mine-item {
+    margin-top: 10px;
+  }
+
+  .status {
+    background-color: #fff;
+    border-radius: 10px;
+    margin-left: 10px;
+    margin: 10px;
+    padding: 10px;
+
+    .status-top {
+      margin-bottom: 10px;
+      display: flex;
+      flex-direction: row;
+
+      .button {
+        margin-right: 10px;
+        height: 20px;
+        float: right;
+        font-size: 10px;
+        border-radius: 20px;
+      }
+
+      .status-title {
+        margin-left: 10px;
+        font-size: 10px;
+        flex: 1;
+      }
+    }
+
+    .status-bottom {
+      margin-top: 10px;
+      display: flex;
+      flex-direction: row;
+
+      .status-item {
+        width: 33%;
+        font-size: 10px;
+        text-align: center;
+      }
+    }
+  }
+}
 </style>
