91c774a7c02c2042e2f640ba143fc48b8f2f2f4a
351510443@qq.com <351510443@qq.com>
2020-11-03 21:50:44 +0800

    updata

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ Vue.config.productionTip = false
 Vue.prototype.get = get;
 Vue.prototype.post = post;
 
+// 将全局样式文件写在 main.js
+import '@/assets/scss/iconfont.scss'
+import '@/assets/ali-fonts/iconfont.css'
+
 //导入mint-ui
 import MintUI from "mint-ui";
 import "mint-ui/lib/style.css";
