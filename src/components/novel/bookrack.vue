fcaba9561d30b395cdd40dd499c46f3b0e18fdde
351510443@qq.com <351510443@qq.com>
2020-11-03 21:28:46 +0800

    updata 书架

--- src/components/novel/bookrack.vue
+++ src/components/novel/bookrack.vue
@@ -1,24 +1,29 @@
 <template>
  <div>
    <mt-navbar v-model="selected">
-     <mt-tab-item id="1">选项一</mt-tab-item>
-     <mt-tab-item id="2">选项二</mt-tab-item>
-     <mt-tab-item id="3">选项三</mt-tab-item>
+     <mt-tab-item id="1">收藏</mt-tab-item>
+     <mt-tab-item id="2">书单</mt-tab-item>
+     <mt-tab-item id="3">历史</mt-tab-item>
+     <mt-tab-item id="4">下载</mt-tab-item>
    </mt-navbar>
-   <div>
+   <!--<div>
      <mt-cell class="page-part" title="当前选中">{{ selected }}</mt-cell>
-   </div>
+   </div>-->
 
    <!-- tabcontainer -->
    <mt-tab-container v-model="selected">
      <mt-tab-container-item id="1">
-       <mt-cell v-for="n in 10" :title="'内容 ' + n" :key="n"/>
+<!--       <mt-cell v-for="n in 10" :title="'内容 ' + n" :key="n"/>-->
+       收藏
      </mt-tab-container-item>
      <mt-tab-container-item id="2">
-       <mt-cell v-for="n in 4" :title="'测试 ' + n" :key="n"/>
+      书单
      </mt-tab-container-item>
      <mt-tab-container-item id="3">
-       <mt-cell v-for="n in 6" :title="'选项 ' + n" :key="n" />
+       历史
+     </mt-tab-container-item>
+     <mt-tab-container-item id="4">
+       下载
      </mt-tab-container-item>
    </mt-tab-container>
  </div>
